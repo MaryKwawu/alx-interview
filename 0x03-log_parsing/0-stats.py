e89e1737b30f417ed255480627197b77c92a0e8d
MaryKwawu <may419316@gmail.com>
2023-10-17 16:51:05 +0000

    added files

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -11,10 +11,11 @@ where <total size> is the sum of all previous <file size>
 (see input format above)
 Number of lines by status code:
 possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-if a status code doesn’t appear or is not an integer,
-don’t print anything for this status code
+if a status code doesn't appear or is not an integer,
+don't print anything for this status code
 format: <status code>: <number>
 status codes should be printed in ascending order
+
 
 line list = [<IP Address>, -, [<date>], "GET /projects/260 HTTP/1.1",
 <status code>, <file size>]
